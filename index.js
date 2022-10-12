e125381dcf3ec965c8cb18d84650a88dca5a87ca
Brandon Castro <castroyou999@gmail.com>
2022-10-12 13:07:45 -0400

    commented out innerHTML and replaced with multiple element initialization + append

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     let drinksList = document.getElementById("drinks list");
     let submitBtn = document.getElementById("submit");
-    let search = document.getElementById('search');
-    let btns = Array.from(document.getElementsByClassName('btn'));
+    // let search = document.getElementById('search');
+
     let searchForm = document.getElementById('search form');
     searchForm.addEventListener('submit',e=>{
         e.preventDefault();
         let searchValue = e.target.search.value;
         if(searchValue !== ''){
-            clearList()
+            drinksList.replaceChildren()
             fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`)
             .then(resp=>resp.json())
             .then(json => {
@@ -18,34 +18,52 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 else json.drinks.map(pullRecipe);
                 searchForm.reset();
             })
+            .catch(error => alert(`Whoops, something went wrong: ${error}`))
         }
     })
 
     // should add this to css instead
-    console.log(btns)
+    
     // const backgroundColor = target.style.backgroundColor
     const setColorCyan = e=>(e.target.style.backgroundColor = 'cyan');
     const setColorRed = e=>(e.target.style.backgroundColor = 'red');
     const setColorDefault = e => (e.target.style.backgroundColor = '');
 
-    btns[0].addEventListener('mouseover', setColorCyan)
-    btns[0].addEventListener('mouseout', setColorDefault)
+    submitBtn.addEventListener('mouseover', setColorCyan)
+    submitBtn.addEventListener('mouseout', setColorDefault)
     // document.getElementsByClassName('btn').addEventListener('mouseover', e=>{
     // })
 
     // function searchForDrinks()
     
-    function clearList(){
-        while(drinksList.firstChild){
-            drinksList.removeChild(drinksList.firstChild)
-        }
-    }
     function pullRecipe(recipe){
         let section = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let img = document.createElement('img');
+        let inputRemove = document.createElement('input');
+        let ol = document.createElement('ol');
+        let liCategory = document.createElement('li');
+        let liAlcohol = document.createElement('li');
+        let liContainer = document.createElement('li');
+        let ul = document.createElement('ul');
+        let p = document.createElement('p');
+        
+        h2.textContent = `${recipe.strDrink}`;
+        img.src = `${recipe.strDrinkThumb}`;
+        inputRemove.type = `button`;
+        inputRemove.className = `btn remove`;
+        inputRemove.value = `remove`;
+        liCategory.textContent = `${recipe.strCategory}`;
+        liAlcohol.textContent = `${recipe.strAlcoholic}`;
+        liContainer.textContent = `${recipe.strGlass}`;
+        p.textContent = `${recipe.strInstructions}`;
+        ol.append(liCategory,liAlcohol,liContainer);
+        section.append(h2,img,inputRemove,ol,ul,p);
 
-        section.innerHTML = `<h2> ${recipe.strDrink} </h2> 
-        <img src='${recipe.strDrinkThumb}'/> <input type='button' class='btn remove' value='Remove'/> 
-        <ol> <li>${recipe.strCategory}</li> <li>${recipe.strAlcoholic}</li> <li>${recipe.strGlass}</li> </ol> <ul> </ul> <p>${recipe.strInstructions}</p>`;
+
+        // section.innerHTML = `<h2> ${recipe.strDrink} </h2> 
+        // <img src='${recipe.strDrinkThumb}'/> <input type='button' class='btn remove' value='Remove'/> 
+        // <ol> <li>${recipe.strCategory}</li> <li>${recipe.strAlcoholic}</li> <li>${recipe.strGlass}</li> </ol> <ul> </ul> <p>${recipe.strInstructions}</p>`;
         section.id = `${recipe.idDrink}`;
         section.className = 'search section';
         for(let i=1;i<15;i++){
@@ -64,6 +82,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
         console.log(recipe.strDrink);
         drinksList.append(section)
     }
+
+    // function addDeleteBtnEvents(){
+    // }
+
     const deleteParent = (e) => e.target.parentNode.remove();
     
     
@@ -71,7 +93,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     // function multiEventListener(element, events,cb){
     //     events.forEach(event => element.addEventListener(event, cb))
     // }
-    
+
     // array of objects?
     // let drinksArray = [
     //     { name: 'Example Drink',
