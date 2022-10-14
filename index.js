f97d7bba7758ca499e9aff6131f7790b0eae238a
Brandon Castro <castroyou999@gmail.com>
2022-10-14 18:12:09 -0400

    added text transform for dark mode button, removed redundant code comments and unused classes

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     let drinksList = document.getElementById("drinks list");
-    let submitBtn = document.getElementById("submit");
     let darkBtn = document.getElementById('dark');
-
     let searchForm = document.getElementById('search form');
-   
 
     function searchFetch(e){
         e.preventDefault();
@@ -16,29 +13,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 if(json.drinks===null){
                     alert("Whoops, no results! Try another search.")
                 }
-                else drinksList.innerHTML = '';
-                json.drinks.map(pullRecipe);
+                else {drinksList.innerHTML = '';
+                json.drinks.map(pullRecipe);}
                 searchForm.reset();
             })
             .catch(error => alert(`Whoops, something went wrong: ${error}`))
-        }
-    
+        }    
     };
 
-    // should add this to css instead
-    
-    // const backgroundColor = target.style.backgroundColor
-    const setColorCyan = e=>(e.target.style.backgroundColor = 'cyan');
-    const setColorRed = e=>(e.target.style.backgroundColor = 'red');
-    const setColorDefault = e => (e.target.style.backgroundColor = '');
-
-    submitBtn.addEventListener('mouseover', setColorCyan)
-    submitBtn.addEventListener('mouseout', setColorDefault)
-    // document.getElementsByClassName('btn').addEventListener('mouseover', e=>{
-    // })
-
-    // function searchForDrinks()
-    
     function pullRecipe(recipe){
         // initialize elements
         let section = document.createElement('section');
@@ -56,21 +38,15 @@ document.addEventListener('DOMContentLoaded', ()=>{
         img.src = recipe.strDrinkThumb;
         img.className = 'drinkImg';
         inputRemove.type = `button`;
-        inputRemove.className = `btn remove`;
+        inputRemove.className = `btnremove`;
         inputRemove.value = `remove`;
         liCategory.textContent = `Category: ${recipe.strCategory}`;
         liAlcohol.textContent = `Alcohol: ${recipe.strAlcoholic}`;
         liContainer.textContent = `Container: ${recipe.strGlass}`;
         p.textContent = recipe.strInstructions;
+        section.id = `${recipe.idDrink}`;
         ol.append(liCategory,liAlcohol,liContainer);
         section.append(h2,img,inputRemove,ol,ul,p);
-
-        // section.innerHTML = `<h2> ${recipe.strDrink} </h2> 
-        // <img src='${recipe.strDrinkThumb}'/> <input type='button' class='btn remove' value='Remove'/> 
-        // <ol> <li>${recipe.strCategory}</li> <li>${recipe.strAlcoholic}</li> <li>${recipe.strGlass}</li> </ol> <ul> </ul> <p>${recipe.strInstructions}</p>`;
-        
-        section.id = `${recipe.idDrink}`;
-        section.className = 'search section';
         for(let i=1;i<15;i++){
             if(recipe[`strIngredient${i}`] !==null){
                 let ingred = document.createElement('li');
@@ -81,25 +57,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 ingredList.append(ingred);
             };
         };
-        let removeBtn = section.querySelector('.remove');
+        let removeBtn = section.querySelector('.btnremove');
         removeBtn.addEventListener('click',deleteParent);
-        removeBtn.addEventListener('mouseover',setColorRed);
-        removeBtn.addEventListener('mouseout',setColorDefault);
         console.log(recipe.strDrink);
         drinksList.append(section);
-    }
+    };
 
     // function addDeleteBtnEvents(){
     // }
 
-    const deleteParent = (e) => e.target.parentNode.remove();
-    
-    
-    // find a better way to do this
-    // function multiEventListener(element, events,cb){
-    //     events.forEach(event => element.addEventListener(event, cb))
-    // }
-
+    function deleteParent(e){
+        e.target.parentNode.remove();
+    };
     // array of objects?
     // let drinksArray = [
     //     { name: 'Example Drink',
@@ -112,7 +81,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
     //         ingredient3: 'Measure',
     //     },
     //     instructions: 'Mixing Instructions'
-
     //     }]
 
     // function createDrink
@@ -120,10 +88,16 @@ document.addEventListener('DOMContentLoaded', ()=>{
     function toggleDarkMode(){
         document.body.classList.toggle('darkmode')
     };
+    function resetDarkBtnText(){
+        darkBtn.textContent = 'Toggle Dark Mode'
+    }
 
     searchForm.addEventListener('submit', searchFetch);
     darkBtn.addEventListener('click',toggleDarkMode);
-
+    document.addEventListener('transitionstart', () => {
+        darkBtn.textContent = "Switching Dark Mode...";
+        setTimeout(resetDarkBtnText , 2000);
+    })
 
     console.log("the dom is now loaded")
 })
